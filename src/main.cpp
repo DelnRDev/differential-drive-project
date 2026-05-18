2dadf93b5622f7fb874b9144e72a0f2156a63255
DelnRDev <81700126+DelnRDev@users.noreply.github.com>
2026-05-18 18:35:14 +0000

    v1

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-
-
 
 double clampVal(double lowerBound, double upperBound, double val){
 
@@ -16,119 +9,94 @@ double clampVal(double lowerBound, double upperBound, double val){
   }
 
   return val;
-
+  
 }
-
-double wrapAngleDeg(double angle){
-
-  while(angle < -180){
-    angle += 360;
-  }
-
-  while (angle >= 180){
-    angle -= 360;
-  }
-
-  return angle;
-
-}
-
-double wrapAngleRad(double angle){
-
-  while(angle < -PI){
-    angle += 2 * PI;
-  }
-
-  while (angle >= PI){
-    angle -= 2 * PI;
-  }
-
-  return angle;
-
-}
-
-
 
 class Encoder{
-public:
-  int encoderPin;
-  int pulsePerRev = 40;
+private:
   volatile long totalPulseCount = 0;
 
-  //ISR
-  unsigned long prevPulseCheckTime = 0;
-  unsigned long incrementDebounceTime = 0;
-
-  //RPM
-  long prevPulseCount = 0;
-  double currentRPM = 0.0;
-  //double prevRPM = 0.0;
-
   int direction = 1;
+  long prevPulseCount = 0;
+  double rpm = 0.0;
+
+public:
+
+  int encoderPin;
+
+  //pulse interrupt
+  unsigned long prevPulseTime = 0;
+  long debonceTime = 500;
+
+  const int pulsePerRev = 40; //checkByHand
+
+
 
   Encoder(int pin):encoderPin(pin){
 
   }
 
   void begin(){
-
     pinMode(encoderPin, INPUT_PULLUP);
-
   }
 
   void pulseIncrement(){
-
+    
     unsigned long currentTime = micros();
 
-    if(currentTime - prevPulseCheckTime > incrementDebounceTime){
-
+    if(currentTime - prevPulseTime >= debonceTime){
       totalPulseCount++;
-
-      prevPulseCheckTime = currentTime;
+      prevPulseTime = currentTime;
     }
 
   }
 
   void updateRPM(double dt){
-
+    
     noInterrupts();
     long currentPulseCount = totalPulseCount;
     interrupts();
-    //Serial.println(currentPulseCount);
-    long deltaPulseCount = currentPulseCount - prevPulseCount;
 
-    currentRPM = (deltaPulseCount / (double)pulsePerRev) * (60.0 / dt);
-     Serial.println(currentRPM);
+    long deltaPulseCount = currentPulseCount - prevPulseCount;
+    
+    rpm = direction * (deltaPulseCount / (double)pulsePerRev) * (60.0 / dt);
 
     prevPulseCount = currentPulseCount;
   }
 
   double getRPM(){
-
-    return currentRPM;
-    
+    return rpm;
   }
 
+  void setDirection(int dir){
+    direction = dir; //1 = forward, -1 = backward
+  }
 };
 
 class Motor{
+private:
+  
 public:
-
-  Encoder* encoder;
 
   int pin1;
   int pin2;
   int pwmPin;
 
-  int direction = 1;
+  Encoder* encoder;
 
-  //setMotorByRPM
-  int minRPM = 30;
-  int maxRPM = 255;
-  double prevRPM = 0.0;
+  //Speed control
+  const int minRPM = 100;
+  const int minPWM = 40;
+  const int maxPWM = 255;
+  const int maxIntegral = 500;
+
   double errorIntegral = 0.0;
+  double prevRPMError = 0.0;
 
-  Motor(int a, int b, int pwm,  Encoder* edr):pin1(a), pin2(b), pwmPin(pwm), encoder(edr){
+
+
+
+  Motor(int a, int b, int pwm, Encoder* edr):pin1(a), pin2(b), pwmPin(pwm), encoder(edr){
 
   }
 
@@ -139,16 +107,14 @@ public:
   }
 
   void setMotorByPWM(int pwm){
-
+    //comeback verify direction experimentally
     if(pwm > 0){
-      direction = 1;
-      encoder->direction = 1;
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, HIGH);
+      encoder->setDirection(1);
+      digitalWrite(pin1, HIGH);
+      digitalWrite(pin2, LOW);
       analogWrite(pwmPin, pwm);
-    }else if (pwm < 0){
-      direction = -1;
-      encoder->direction = -1;
+    }else if(pwm < 0){
+      encoder->setDirection(-1);
       digitalWrite(pin1, HIGH);
       digitalWrite(pin2, LOW);
       analogWrite(pwmPin, -pwm);
@@ -157,102 +123,62 @@ public:
       digitalWrite(pin2, LOW);
       analogWrite(pwmPin, 0);
     }
-
   }
-  
+
   void setMotorByRPM(int targetRPM, double dt){
-    //test this function
+
+    if(abs(targetRPM) < minRPM){
+      setMotorByPWM(0);
+      return;
+    }
+
+    double Kp = 1.0;
+    double Ki = 0.2;
+    double Kd = 0.1;
+    double Kv = 0.1;
 
     double currentRPM = encoder->getRPM();
-
-    double Kp = 2.0;
-    double Ki = 0.1;
-
     double rpmError = targetRPM - currentRPM;
 
-    double maxIntegralRPMContribution = 40;
-    
     errorIntegral += rpmError * dt;
+    errorIntegral = clampVal(-maxIntegral, maxIntegral, errorIntegral);
 
-    //prevent intgral windup
-    if(errorIntegral > 0 && Ki * errorIntegral > maxIntegralRPMContribution){
-      errorIntegral = maxIntegralRPMContribution / Ki;
-    }else if(errorIntegral < 0 && Ki * Ki * errorIntegral < -maxIntegralRPMContribution ){
-      errorIntegral = -maxIntegralRPMContribution / Ki;
+    double errorDerivative = (rpmError - prevRPMError) / dt;
+
+    double u = Kp * rpmError + Ki * errorIntegral + Kd * errorDerivative + Kv * targetRPM;
+
+    u = clampVal(-maxPWM, maxPWM, u);
+
+    if(u > 0 && u < minPWM){
+      u = minPWM;
+    }else if(u < 0 && u < -minPWM){
+      u = -minPWM;
     }
 
-    double u = Kp * rpmError + Ki * errorIntegral;
-
-    u = clampVal(-maxRPM, maxRPM, u);
-
-    if(u > 0 && u < minRPM){
-      u = minRPM;
-    }else if(u < 0 && u > -minRPM){
-      u = -minRPM;
-    }
-    Serial.println(currentRPM);
     setMotorByPWM(u);
 
-
-
-
-  }
-  
-
-};
-
-
-class Drivetrain{
-public:
-
-  Encoder* lEdr;
-  Encoder* rEdr;
-  Motor* lMtr;
-  Motor* rMtr;
-
-  Drivetrain(Encoder* leftEdr, Encoder* rightEdr, Motor* leftMtr, Motor* rightMtr):lEdr(leftEdr), rEdr(rightEdr), lMtr(leftMtr), rMtr(rightMtr){
-
-  }
-
-  void begin(){
-
-    lEdr->begin();
-    rEdr->begin();
-    lMtr->begin();
-    rMtr->begin();
-
-  }
-
-  void setDriveByPWM(int leftPWM, int rightPWM){
-
-    lMtr->setMotorByPWM(leftPWM);
-    rMtr->setMotorByPWM(rightPWM);
-
+    prevRPMError = rpmError;
   }
 
 };
 
 
-int LEncoderPin = 3;
-int REncoderPin = 2;
-
-int AIN1 = 8;
-int AIN2 = 7;
-int PWMA = 5;
-
-int BIN1 = 9;
-int BIN2 = 10;
-int PWMB = 6;
-
-Adafruit_MPU6050 mpu;
-
+//======================================
+int LEncoderPin;
+int REncoderPin;
+int AIN1;
+int AIN2;
+int PWMA;
+int BIN1;
+int BIN2;
+int PWMB;
+//======================================
 Encoder leftEncoder(LEncoderPin);
 Encoder rightEncoder(REncoderPin);
 Motor leftMotor(BIN1, BIN2, PWMB, &leftEncoder);
-Motor rightMotor(AIN2, AIN1, PWMA, &rightEncoder);
+Motor rightMotor(AIN1, AIN2, PWMA, &rightEncoder);
 
-Drivetrain drivetrain(&leftEncoder, &rightEncoder, &leftMotor, &rightMotor);
-
+//======================================
 
 void leftEncoderISR(){
   leftEncoder.pulseIncrement();
@@ -262,40 +188,45 @@ void rightEncoderISR(){
   rightEncoder.pulseIncrement();
 }
 
-unsigned long prevEncoderUpdateTime;
+//======================================
+unsigned long prevRPMTime;
+unsigned long prevControlTime;
 
-double encoderPeriod = 10000;
+unsigned long rpmUpdatePeriod = 100000;
+unsigned long controlUpdatePeriod = 100000;
+//======================================
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
-void setup() {
-
-  prevEncoderUpdateTime = micros();
-
-  Serial.begin(115200);
+void setup(){
   
+  Serial.begin(115200);
+  delay(1000);
 
+  
   attachInterrupt(digitalPinToInterrupt(LEncoderPin), leftEncoderISR, FALLING);
   attachInterrupt(digitalPinToInterrupt(REncoderPin), rightEncoderISR, FALLING);
-}
-
-unsigned long updatePIDPeriod = 10000;
-unsigned long updateEncoderPeriod = 50000;
-
-void loop() {
-  /*
+  
   unsigned long currentTime = micros();
-  //Serial.println(currentTime - prevEncoderUpdateTime);
-  if(currentTime - prevEncoderUpdateTime >= updateEncoderPeriod){
-    double dt = (currentTime - prevEncoderUpdateTime)/ 1000000.0;
-    leftEncoder.updateRPM(dt);
 
-    //Serial.println(leftEncoder.getRPM());
-    leftMotor.setMotorByRPM(200,dt);
-    prevEncoderUpdateTime = currentTime;
-  }
-  */
-  Serial.println(leftEncoder.totalPulseCount);
- 
+  prevRPMTime = currentTime;
+  
 }
 
+void loop(){
+
+  unsigned long currentTime = micros();
+  
+  
+  if(currentTime - prevRPMTime >= rpmUpdatePeriod){
+    double dt = (currentTime - prevRPMTime) / 1000000.0;
+    leftEncoder.updateRPM(dt);
+    prevRPMTime = currentTime;
+  }
+
+  if(currentTime - prevControlTime >= controlUpdatePeriod){
+    double dt = (currentTime - prevControlTime) / 1000000.0;
+    leftMotor.setMotorByRPM(200, dt);
+    prevControlTime = currentTime;
+  }
+
+}
