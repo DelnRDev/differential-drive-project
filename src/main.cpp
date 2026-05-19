662c8ac98fd81bcdb01c7aabbe3405ac9819f3da
DelnRDev <81700126+DelnRDev@users.noreply.github.com>
2026-05-19 00:59:05 +0000

    v1

--- src/main.cpp
+++ src/main.cpp
@@ -1,301 +1,167 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-
-
-
-double clampVal(double lowerBound, double upperBound, double val){
-
-  if(val < lowerBound){
-    return lowerBound;
-  }else if(val > upperBound){
-    return upperBound;
-  }
-
-  return val;
-
-}
-
-double wrapAngleDeg(double angle){
-
-  while(angle < -180){
-    angle += 360;
-  }
-
-  while (angle >= 180){
-    angle -= 360;
-  }
-
-  return angle;
-
-}
-
-double wrapAngleRad(double angle){
-
-  while(angle < -PI){
-    angle += 2 * PI;
-  }
-
-  while (angle >= PI){
-    angle -= 2 * PI;
-  }
-
-  return angle;
-
-}
-
-
 
 class Encoder{
 public:
-  int encoderPin;
-  int pulsePerRev = 40;
-  volatile long totalPulseCount = 0;
 
-  //ISR
-  unsigned long prevPulseCheckTime = 0;
-  unsigned long incrementDebounceTime = 0;
+   int encoderPin;
+   volatile long totalPulseCount = 0;
 
-  //RPM
-  long prevPulseCount = 0;
-  double currentRPM = 0.0;
-  //double prevRPM = 0.0;
+   const int pulsePerRev = 20;
 
-  int direction = 1;
+   //ISR
+    unsigned long debounceTime = 500;
+    unsigned long prevPulseUpdateTime = 0;
 
-  Encoder(int pin):encoderPin(pin){
+    //RPM
+    double rpm = 0.0;
+    long prevPulseCountRPM = 0;
 
-  }
+    //Odom
+    double wheelRadius = 1.0;
+    long prevPulseCountOdom = 0;
 
-  void begin(){
 
-    pinMode(encoderPin, INPUT_PULLUP);
+   Encoder(int pin):encoderPin(pin){
+    
+   }
 
-  }
-
-  void pulseIncrement(){
+   void pulseIncrement(){
 
     unsigned long currentTime = micros();
 
-    if(currentTime - prevPulseCheckTime > incrementDebounceTime){
-
+    if(currentTime - prevPulseUpdateTime >= debounceTime){
       totalPulseCount++;
-
-      prevPulseCheckTime = currentTime;
+      prevPulseUpdateTime = currentTime;
     }
 
-  }
+   }
 
-  void updateRPM(double dt){
+   void updateRPM(double dt){
 
     noInterrupts();
     long currentPulseCount = totalPulseCount;
     interrupts();
-    //Serial.println(currentPulseCount);
-    long deltaPulseCount = currentPulseCount - prevPulseCount;
-
-    currentRPM = (deltaPulseCount / (double)pulsePerRev) * (60.0 / dt);
-     Serial.println(currentRPM);
-
-    prevPulseCount = currentPulseCount;
-  }
-
-  double getRPM(){
-
-    return currentRPM;
     
-  }
+    long deltaPulseCount = currentPulseCount - prevPulseCountRPM;
+
+    rpm = (deltaPulseCount / (double)pulsePerRev) * ((60.0) / dt);
+
+   }
+   
+   double getDeltaS(double dt){//only for odom, maybe we can put into odom class
+    
+    noInterrupts();
+    long currentPulseCount = totalPulseCount;
+    interrupts();
+    
+    long deltaPulseCount = currentPulseCount - prevPulseCountOdom;
+
+    return (deltaPulseCount / (double)pulsePerRev) * 2 * PI * wheelRadius;
+
+   }
 
 };
 
 class Motor{
 public:
 
-  Encoder* encoder;
 
-  int pin1;
-  int pin2;
-  int pwmPin;
-
-  int direction = 1;
-
-  //setMotorByRPM
-  int minRPM = 30;
-  int maxRPM = 255;
-  double prevRPM = 0.0;
-  double errorIntegral = 0.0;
-
-  Motor(int a, int b, int pwm,  Encoder* edr):pin1(a), pin2(b), pwmPin(pwm), encoder(edr){
-
-  }
-
-  void begin(){
-    pinMode(pin1, OUTPUT);
-    pinMode(pin2, OUTPUT);
-    pinMode(pwmPin, OUTPUT);
-  }
-
-  void setMotorByPWM(int pwm){
-
-    if(pwm > 0){
-      direction = 1;
-      encoder->direction = 1;
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, HIGH);
-      analogWrite(pwmPin, pwm);
-    }else if (pwm < 0){
-      direction = -1;
-      encoder->direction = -1;
-      digitalWrite(pin1, HIGH);
-      digitalWrite(pin2, LOW);
-      analogWrite(pwmPin, -pwm);
-    }else{
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, LOW);
-      analogWrite(pwmPin, 0);
-    }
-
-  }
-  
-  void setMotorByRPM(int targetRPM, double dt){
-    //test this function
-
-    double currentRPM = encoder->getRPM();
-
-    double Kp = 2.0;
-    double Ki = 0.1;
-
-    double rpmError = targetRPM - currentRPM;
-
-    double maxIntegralRPMContribution = 40;
-    
-    errorIntegral += rpmError * dt;
-
-    //prevent intgral windup
-    if(errorIntegral > 0 && Ki * errorIntegral > maxIntegralRPMContribution){
-      errorIntegral = maxIntegralRPMContribution / Ki;
-    }else if(errorIntegral < 0 && Ki * Ki * errorIntegral < -maxIntegralRPMContribution ){
-      errorIntegral = -maxIntegralRPMContribution / Ki;
-    }
-
-    double u = Kp * rpmError + Ki * errorIntegral;
-
-    u = clampVal(-maxRPM, maxRPM, u);
-
-    if(u > 0 && u < minRPM){
-      u = minRPM;
-    }else if(u < 0 && u > -minRPM){
-      u = -minRPM;
-    }
-    Serial.println(currentRPM);
-    setMotorByPWM(u);
-
-
-
-
-  }
-  
 
 };
 
+class Odometry{
+public:
+  
+  Encoder* lEdr;
+  Encoder* rEdr;
+
+  const double trackWidth = 1.0;
+
+  Odometry(Encoder* leftEdr, Encoder* rightEdr): lEdr(leftEdr), rEdr(rightEdr){
+
+  }
+
+struct{
+  double x = 0.0;
+  double y = 0.0;
+  double theta = 0.0;
+}currentPose;
+
+  //purely from encoder
+  void updatePose(double dt){
+    
+    double deltaSL = lEdr->getDeltaS(dt);
+    double deltaSR = rEdr->getDeltaS(dt);
+
+    double deltaSCM = (deltaSL + deltaSR) / 2;
+    double deltaTheta = (deltaSR - deltaSL) / trackWidth;
+
+    currentPose.x += deltaSCM * cos(currentPose.theta);
+    currentPose.y += deltaSCM * sin(currentPose.theta);
+    currentPose.theta += deltaTheta;
+
+  }
+
+};
 
 class Drivetrain{
 public:
 
-  Encoder* lEdr;
-  Encoder* rEdr;
-  Motor* lMtr;
-  Motor* rMtr;
-
-  Drivetrain(Encoder* leftEdr, Encoder* rightEdr, Motor* leftMtr, Motor* rightMtr):lEdr(leftEdr), rEdr(rightEdr), lMtr(leftMtr), rMtr(rightMtr){
-
-  }
-
-  void begin(){
-
-    lEdr->begin();
-    rEdr->begin();
-    lMtr->begin();
-    rMtr->begin();
-
-  }
-
-  void setDriveByPWM(int leftPWM, int rightPWM){
-
-    lMtr->setMotorByPWM(leftPWM);
-    rMtr->setMotorByPWM(rightPWM);
-
-  }
-
 };
-
-
-int LEncoderPin = 3;
-int REncoderPin = 2;
-
-int AIN1 = 8;
-int AIN2 = 7;
-int PWMA = 5;
-
-int BIN1 = 9;
-int BIN2 = 10;
-int PWMB = 6;
-
-Adafruit_MPU6050 mpu;
+//==========================================
+int LEncoderPin;
+int REncoderPin;
+//==========================================
 
 Encoder leftEncoder(LEncoderPin);
 Encoder rightEncoder(REncoderPin);
-Motor leftMotor(BIN1, BIN2, PWMB, &leftEncoder);
-Motor rightMotor(AIN2, AIN1, PWMA, &rightEncoder);
 
-Drivetrain drivetrain(&leftEncoder, &rightEncoder, &leftMotor, &rightMotor);
-
+Odometry odom(&leftEncoder, &rightEncoder);
+//==========================================
 
 void leftEncoderISR(){
+
   leftEncoder.pulseIncrement();
+
 }
 
 void rightEncoderISR(){
+
   rightEncoder.pulseIncrement();
+
 }
 
-unsigned long prevEncoderUpdateTime;
+//==========================================
 
-double encoderPeriod = 10000;
+unsigned long prevTime;
+//==========================================
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
-
-void setup() {
-
-  prevEncoderUpdateTime = micros();
-
-  Serial.begin(115200);
+void setup(){
   
+  Serial.begin(115200);
+  delay(1000);
+
 
   attachInterrupt(digitalPinToInterrupt(LEncoderPin), leftEncoderISR, FALLING);
   attachInterrupt(digitalPinToInterrupt(REncoderPin), rightEncoderISR, FALLING);
-}
-
-unsigned long updatePIDPeriod = 10000;
-unsigned long updateEncoderPeriod = 50000;
-
-void loop() {
-  /*
+  
   unsigned long currentTime = micros();
-  //Serial.println(currentTime - prevEncoderUpdateTime);
-  if(currentTime - prevEncoderUpdateTime >= updateEncoderPeriod){
-    double dt = (currentTime - prevEncoderUpdateTime)/ 1000000.0;
-    leftEncoder.updateRPM(dt);
-
-    //Serial.println(leftEncoder.getRPM());
-    leftMotor.setMotorByRPM(200,dt);
-    prevEncoderUpdateTime = currentTime;
-  }
-  */
-  Serial.println(leftEncoder.totalPulseCount);
- 
+  prevTime = currentTime;
 }
 
+
+void loop(){
+
+  unsigned long currentTime = micros();
+
+  double dt = (currentTime - prevTime);
+  //updateRPM
+
+  //updateOdm
+  odom.updatePose(dt);
+
+  //updateControl
+
+  prevTime = currentTime;
+
+}
