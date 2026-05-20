be4c27ba79b2c4a7fbbede3499ffd32441d56c99
DelnRDev <samuelhui2004@gmail.com>
2026-05-20 15:48:09 -0700

    added auton, multirated system, and some other updates

--- src/main.cpp
+++ src/main.cpp
@@ -51,20 +51,7 @@ double wrapAngleRad(double angle){
 
 class Encoder{
 public:
-  int encoderPin;
-  int pulsePerRev = 40;
-  volatile long totalPulseCount = 0;
-
-  //ISR
-  unsigned long prevPulseCheckTime = 0;
-  unsigned long incrementDebounceTime = 0;
-
-  //RPM
-  long prevPulseCount = 0;
-  double currentRPM = 0.0;
-  //double prevRPM = 0.0;
-
-  int direction = 1;
+  
 
   Encoder(int pin):encoderPin(pin){
 
@@ -80,11 +67,11 @@ public:
 
     unsigned long currentTime = micros();
 
-    if(currentTime - prevPulseCheckTime > incrementDebounceTime){
+    if(currentTime - prevPulseUpdateTime >= incrementDebounceTime){
 
       totalPulseCount++;
 
-      prevPulseCheckTime = currentTime;
+      prevPulseUpdateTime = currentTime;
     }
 
   }
@@ -94,40 +81,39 @@ public:
     noInterrupts();
     long currentPulseCount = totalPulseCount;
     interrupts();
-    //Serial.println(currentPulseCount);
+   
     long deltaPulseCount = currentPulseCount - prevPulseCount;
 
     currentRPM = (deltaPulseCount / (double)pulsePerRev) * (60.0 / dt);
-     Serial.println(currentRPM);
-
+   
     prevPulseCount = currentPulseCount;
   }
 
-  double getRPM(){
+  double getCurrentRPM(){
 
     return currentRPM;
     
   }
+
+  private:
+    int encoderPin;
+    int pulsePerRev = 40;
+    volatile long totalPulseCount = 0;
+
+    //ISR
+    unsigned long prevPulseUpdateTime = 0;
+    unsigned long incrementDebounceTime = 500;
+
+    //RPM
+    long prevPulseCount = 0;
+    double currentRPM = 0.0;
 
 };
 
 class Motor{
 public:
 
-  Encoder* encoder;
-
-  int pin1;
-  int pin2;
-  int pwmPin;
-
-  int direction = 1;
-
-  //setMotorByRPM
-  int minRPM = 30;
-  int maxRPM = 255;
-  double prevRPM = 0.0;
-  double errorIntegral = 0.0;
-
+  
   Motor(int a, int b, int pwm,  Encoder* edr):pin1(a), pin2(b), pwmPin(pwm), encoder(edr){
 
   }
@@ -142,13 +128,13 @@ public:
 
     if(pwm > 0){
       direction = 1;
-      encoder->direction = 1;
+      direction = 1;
       digitalWrite(pin1, LOW);
       digitalWrite(pin2, HIGH);
       analogWrite(pwmPin, pwm);
     }else if (pwm < 0){
       direction = -1;
-      encoder->direction = -1;
+      direction = -1;
       digitalWrite(pin1, HIGH);
       digitalWrite(pin2, LOW);
       analogWrite(pwmPin, -pwm);
@@ -163,25 +149,25 @@ public:
   void setMotorByRPM(int targetRPM, double dt){
     //test this function
 
-    double currentRPM = encoder->getRPM();
+    double currentRPM = encoder->getCurrentRPM();
 
     double Kp = 2.0;
     double Ki = 0.1;
+    double Kd = 0.0;
+    double Kv = 0.0;
+
+    double uff = Kv * targetRPM + minRPM; // feed forward pwm
 
     double rpmError = targetRPM - currentRPM;
 
     double maxIntegralRPMContribution = 40;
     
     errorIntegral += rpmError * dt;
+    errorIntegral = clampVal(-maxIntegral, maxIntegral, errorIntegral);
+  
+    double errorDerivative = (rpmError - prevRPMError) / dt;
 
-    //prevent intgral windup
-    if(errorIntegral > 0 && Ki * errorIntegral > maxIntegralRPMContribution){
-      errorIntegral = maxIntegralRPMContribution / Ki;
-    }else if(errorIntegral < 0 && Ki * Ki * errorIntegral < -maxIntegralRPMContribution ){
-      errorIntegral = -maxIntegralRPMContribution / Ki;
-    }
-
-    double u = Kp * rpmError + Ki * errorIntegral;
+    double u = Kp * rpmError + Ki * errorIntegral +Kd * errorDerivative + uff;
 
     u = clampVal(-maxRPM, maxRPM, u);
 
@@ -190,14 +176,29 @@ public:
     }else if(u < 0 && u > -minRPM){
       u = -minRPM;
     }
+
     Serial.println(currentRPM);
     setMotorByPWM(u);
 
-
-
-
   }
   
+  private:
+    Encoder* encoder;
+
+    int pin1;
+    int pin2;
+    int pwmPin;
+
+    int direction = 1;
+
+    //motor controller
+    int minRPM = 30;
+    int maxRPM = 255;
+    double maxIntegral = 50.0 ;
+    double prevRPMError = 0.0;
+    double errorIntegral = 0.0;
+
+
 
 };
 
@@ -209,6 +210,8 @@ public:
   Encoder* rEdr;
   Motor* lMtr;
   Motor* rMtr;
+
+  
 
   Drivetrain(Encoder* leftEdr, Encoder* rightEdr, Motor* leftMtr, Motor* rightMtr):lEdr(leftEdr), rEdr(rightEdr), lMtr(leftMtr), rMtr(rightMtr){
 
@@ -253,6 +256,8 @@ Motor rightMotor(AIN2, AIN1, PWMA, &rightEncoder);
 
 Drivetrain drivetrain(&leftEncoder, &rightEncoder, &leftMotor, &rightMotor);
 
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
 
 void leftEncoderISR(){
   leftEncoder.pulseIncrement();
@@ -262,40 +267,109 @@ void rightEncoderISR(){
   rightEncoder.pulseIncrement();
 }
 
+//------------------------------------------------
+
 unsigned long prevEncoderUpdateTime;
+unsigned long prevAutonUpdateTime;
 
-double encoderPeriod = 10000;
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
+unsigned long encoderUpdatePeriod = 10000;
+unsigned long autonUpdatePeriod = 10000;
+//-------------------------------------------------
+enum AutoMode{
+  AUTO_IDLE,
+  AUTO_DRIVE1,
+  AUTO_TURN1,
+  AUTO_DRIVE2,
+  AUTO_TURN2,
+  DONE
+};
+
+AutoMode autoMode = AUTO_IDLE;
+
+
+
+void runAutonomous(double dt){
+  
+  switch(autoMode){
+
+    case(AUTO_DRIVE1):
+      bool completed; //motion cmd
+      if(completed){
+        autoMode = AUTO_TURN1;
+      }
+      break;
+    
+    case(AUTO_TURN1):
+      bool completed; //motion cmd
+      if(completed){
+        //setInitialPose to current state
+        autoMode = AUTO_DRIVE2;
+      }
+      break;
+      
+    case(AUTO_DRIVE2):
+      bool completed; //motion cmd
+      if(completed){
+        autoMode = AUTO_TURN2;
+      }
+      break;
+
+    case(AUTO_TURN2):
+      bool completed; //motion cmd
+      if(completed){
+        autoMode = DONE;
+      }
+      break;
+    
+  }
+
+}
+
+//
 
 void setup() {
 
-  prevEncoderUpdateTime = micros();
-
   Serial.begin(115200);
+  delay(1000);
+  Wire.begin();
+  drivetrain.begin();
   
 
   attachInterrupt(digitalPinToInterrupt(LEncoderPin), leftEncoderISR, FALLING);
   attachInterrupt(digitalPinToInterrupt(REncoderPin), rightEncoderISR, FALLING);
+
+  unsigned long currentTime = micros();
+  prevEncoderUpdateTime = currentTime;
+  prevAutonUpdateTime = currentTime;
 }
 
-unsigned long updatePIDPeriod = 10000;
-unsigned long updateEncoderPeriod = 50000;
 
 void loop() {
-  /*
+ 
   unsigned long currentTime = micros();
-  //Serial.println(currentTime - prevEncoderUpdateTime);
-  if(currentTime - prevEncoderUpdateTime >= updateEncoderPeriod){
-    double dt = (currentTime - prevEncoderUpdateTime)/ 1000000.0;
-    leftEncoder.updateRPM(dt);
 
-    //Serial.println(leftEncoder.getRPM());
-    leftMotor.setMotorByRPM(200,dt);
-    prevEncoderUpdateTime = currentTime;
+  //sense -> think -> act
+  //sensorInput -> process and calculate and predict -> set the actuator
+
+  if(currentTime - prevEncoderUpdateTime >= encoderUpdatePeriod){
+    
+    unsigned long dt = (currentTime - prevAutonUpdateTime) / 1000000.0;
+
+    leftEncoder.updateRPM(dt);
+    rightEncoder.updateRPM(dt);
+
   }
-  */
-  Serial.println(leftEncoder.totalPulseCount);
+
+  //updateOdom here
+
+  if(currentTime - prevAutonUpdateTime >= autonUpdatePeriod){
+
+    unsigned long dt = (currentTime - prevAutonUpdateTime) / 1000000.0;
+
+    runAutonomous(dt);
+
+  }
  
 }
 
